84fbb927ac032debf60c9a66089325a7ceee7c6f
Juan Carlos Galvis Zuluaga <jcgalvis@bancolombia.com.co>
2023-03-15 09:03:49 -0500

    update elixir and deps

--- mix.exs
+++ mix.exs
@@ -37,11 +37,11 @@ defmodule ReactiveCommons.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       {:poison, "~> 5.0"},
-      {:amqp, "~> 1.4"},
+      {:amqp, "~> 3.2"},
       {:uuid, "~> 1.1"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:telemetry, "~> 1.0"},
-      {:mock, "~> 0.3.0", only: :test}
+      {:telemetry, "~> 1.2"},
+      {:mock, "~> 0.3", only: :test}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
